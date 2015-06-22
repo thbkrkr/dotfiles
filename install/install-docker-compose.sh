6fe4aed8d683ae11aac62de0f2e37a1d65f4c285
thbkrkr <thb.krkr@gmail.com>
2015-06-22 20:15:25 +0200

    docker-compose 1.3.1

--- install/install-docker-compose.sh
+++ install/install-docker-compose.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -eu
 #
-# Install docker-compose (https://github.com/docker/compose)
+# Install Docker Compose (https://github.com/docker/compose)
 
-DOCKER_COMPOSE_VERSION=1.3.0
+DOCKER_COMPOSE_VERSION=1.3.1
 DOCKER_COMPOSE_URL=https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-Linux-x86_64
 
 is_up2date() {
@@ -13,8 +13,8 @@ is_up2date() {
 if ! is_up2date
 then
     echo "Install docker-compose v$DOCKER_COMPOSE_VERSION..."
-    sudo wget ${DOCKER_COMPOSE_URL} -O /usr/local/bin/docker-compose && \
-        sudo chmod 755 /usr/bin/docker-compose
+    sudo wget ${DOCKER_COMPOSE_URL} -O /usr/local/bin/docker-compose \
+        && sudo chmod 755 /usr/local/bin/docker-compose
 else
     echo "docker-compose v$DOCKER_COMPOSE_VERSION is up-to-date"
 fi

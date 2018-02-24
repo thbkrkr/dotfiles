ee4e37cbb5730afe843b529f12cca79f7d7c90ab
thbkrkr <thb.krkr@gmail.com>
2018-02-24 20:30:49 +0100

    Install docker-ce

---
+++ install/install-docker-ce.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -eu
+#
+# Install docker-ce on ubuntu (https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu)
+
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
+
+sudo usermod -aG docker $(whoami)
